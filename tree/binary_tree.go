6678ba117a9d33514fac7fcf2be01e880ab3580d
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2024-04-13 21:50:49 +0700

    completed tree structure

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "fmt"
+
 type TreeNode struct {
 	Data  int
 	Left  *TreeNode
@@ -43,5 +45,66 @@ func (t *TreeNode) Min() *TreeNode {
 }
 
 func (t *TreeNode) Delete(data int) *TreeNode {
+	if t == nil {
+		return nil
+	}
+	if t.Data > data {
+		t.Left = t.Left.Delete(data)
+	} else if t.Data < data {
+		t.Right = t.Right.Delete(data)
+	} else {
+		if t.Left == nil {
+			temp := t.Right
+			t = nil 
+			return temp
+		} else if t.Right == nil {
+			temp := t.Left
+			t = nil
+			return temp
+		}
+
+		temp := t.Right.Min()
+		t.Data = temp.Data
+		t.Right = t.Right.Delete(temp.Data)
+	}
 	
+	return t
 } 
+
+func (t *TreeNode) Find(data int) bool {
+	if t == nil {
+		return false
+	}
+
+	if t.Data == data {
+		return true
+	} else if t.Data > data {
+		return t.Left.Find(data)
+	} else {
+		return t.Right.Find(data)
+	}
+}
+
+func (t *TreeNode) PreOrder() {
+	if t != nil {
+		fmt.Printf("%d ", t.Data)
+		t.Left.PreOrder()
+		t.Right.PreOrder()
+	}
+}
+
+func (t *TreeNode) InOrder() {
+	if t != nil {
+		t.Left.InOrder()
+		fmt.Printf("%d ", t.Data)
+		t.Right.InOrder()
+	}
+}
+
+func (t *TreeNode) PostOrder() {
+	if t != nil {
+		t.Left.PostOrder()
+		t.Right.PostOrder()
+		fmt.Printf("%d ", t.Data)
+	}
+}
